fe7fca344b50f2997d177ddda6cc21ca725a9f9c
Simon Waters <simon@kernelize.ai>
2025-08-09 20:29:16 +0000

    - fixed grid stepping in cpu plugin

--- scripts/test_build.sh
+++ scripts/test_build.sh
@@ -22,7 +22,7 @@ main() {
   make -j$(nproc)
 
   printf "Running CPU tests"
-  ./test/cpp/test_basic_kernel cpu kernel_libs/cpu_kernels.so add_vectors
+  ./test/cpp/test_basic_kernel cpu kernel_libs/cpu_kernel.so add_vectors
 
   if [[ "$os_type" == "macos" ]]; then
     printf "Running macOS test"
