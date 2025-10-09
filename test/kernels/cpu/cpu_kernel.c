de6f73b96fb1bd40a32998cf9ee75f3b2acbceb8
Simon Waters <simon@kernelize.ai>
2025-10-09 11:47:24 -0500

    - remove printf

--- test/kernels/cpu/cpu_kernel.c
+++ test/kernels/cpu/cpu_kernel.c
@@ -8,7 +8,5 @@ void add_vectors(float *a, float *b, float *out, int launch_size[],
   a += launch_id[0] * stride;
   b += launch_id[0] * stride;
   out += launch_id[0] * stride;
-  printf("DIMS: %u %u %u -> %f %f %f\n", launch_id[0], launch_id[1],
-         launch_id[2], a[0], b[0], out[0]);
   out[launch_id[3]] = a[launch_id[3]] + b[launch_id[3]];
 }
