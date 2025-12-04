7ee5caa44beb83dcc5a2ae7f18fc80c2cd14defd
Simon Waters <simon@kernelize.ai>
2025-12-04 16:57:55 +0000

    [Plugin] Added TensTorrent tt-metal runtime plugin - uses simple run-length placement algorithm - uses command.set_const() for CB specification - todo: add kernel catalog for CB metadata

---
+++ plugins/tenstorrent/tt_schedule.cpp
@@ -0,0 +1,84 @@
+
+#include "tt_schedule.h"
+
+#include "tt_runtime.h"
+
+float TTSchedule::getTime() const {
+  return std::chrono::duration_cast<std::chrono::milliseconds>(end_time -
+                                                               start_time)
+      .count();
+}
+
+bool placeCommand(nxs_uint cmdSize, ttm::CoreRangeSet &coreRangeSet, ttm::CoreRange &cmdRange, const ttm::CoreCoord &devSize) {
+  auto numRows = (cmdSize / devSize.x) + !!(cmdSize % devSize.x);
+  auto tail = cmdSize % devSize.x;
+
+  // TODO: use this instead
+  // std::optional<CoreRange> select_contiguous_range_from_corerangeset(const CoreRangeSet& crs, uint32_t x, uint32_t y);
+  if (numRows == 1) {
+    // find gap  and return
+
+  }
+  // MUST BE A RECTANGLE
+  size_t nextRow = 0;
+  if (!coreRangeSet.empty())
+    nextRow = coreRangeSet.bounding_box().end_coord.y + 1;
+
+  NXSAPI_LOG(nexus::NXS_LOG_NOTE, "placeCommand: nextRow=", nextRow, ", numRows=", numRows);
+  if (nextRow + numRows > devSize.y)
+    return false;
+
+  size_t endX = (numRows > 1 ? devSize.x : cmdSize) - 1;
+  size_t endY = nextRow + numRows - 1;
+  cmdRange = {{0, nextRow}, {endX, endY}};
+  // ??? how to merge? this doesn't link
+  //coreRangeSet = coreRangeSet.merge(cmdRange);
+  return true;
+}
+
+nxs_status TTSchedule::run(nxs_int stream, nxs_uint run_settings) {
+  NXSAPI_LOG(nexus::NXS_LOG_NOTE,
+             "Schedule::run ");
+
+  nxs_uint settings = getSettings() | run_settings;
+
+  // map commands across cores
+  auto device = getDevice();
+  TT_NOBJ_CHECK(device_range, ttmd::MeshCoordinateRange, device->shape());
+  ttmd::MeshCommandQueue& cq = device->mesh_command_queue();
+  ttmd::MeshWorkload workload;
+
+  // get current device size
+  // TODO: use CoreRangeSet to collect runs
+  auto devGrid = device->logical_grid_size();
+  NXSAPI_LOG(nexus::NXS_LOG_NOTE, "Device grid: ", devGrid.x, ",", devGrid.y);
+
+  ttm::CoreRangeSet coreRangeSet;
+
+  for (auto cmd : getCommands()) {
+    ttm::CoreRange cmdCores {{0,0}, {0,0}};
+    if (!placeCommand(cmd->getGridSize(), coreRangeSet, cmdCores, devGrid)) {
+      //assert(0); // enqueue and start another workload
+    }
+    auto status = cmd->runCommand(stream, workload, device_range, cmdCores);
+    if (!nxs_success(status)) return status;
+  }
+
+  if (settings & NXS_ExecutionSettings_Timing) {
+    start_time = std::chrono::steady_clock::now();
+  }
+
+  TT_CHECK(ttmd::EnqueueMeshWorkload, cq, workload, false);
+  TT_CHECK(ttmd::Finish, cq);
+
+
+  if (settings & NXS_ExecutionSettings_Timing) {
+    end_time = std::chrono::steady_clock::now();
+  }
+  return NXS_Success;
+}
+
+nxs_status TTSchedule::release() {
+  nxs_status status = Schedule::release();
+  return status;
+}
